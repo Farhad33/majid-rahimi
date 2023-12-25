5462d1d518271aaf10a96d9dc7e9c099b14e1a47
Majid Rahimi <majid88rahimi@gmail.com>
2023-12-24 16:36:34 -0800

    First commit

---
+++ src/app/Header.js
@@ -0,0 +1,15 @@
+'use client'
+import styled from 'styled-components'
+
+export default function Header({ Component, pageProps }) {
+    return (
+        <Work>Work in progress...</Work>
+    )
+}
+
+const Work = styled.h1`
+    text-align: center;
+    font-size: 40px;
+    color: red;
+    margin: 20px;
+`
