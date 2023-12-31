3cc777d5393fc0efa1d1951cece061c616bceb32
Majid Rahimi <majid88rahimi@gmail.com>
2023-12-30 18:15:45 -0800

    Made some minor changes

--- src/app/algorithms/[title]/page.js
+++ src/app/algorithms/[title]/page.js
@@ -1,96 +1,17 @@
-'use client'
-import React, { useState, useEffect } from'react'
-import Styled from 'styled-components'
-import Editor from "@monaco-editor/react";
-import SidePanel from '@/components/SidePanel'
-import Container from '@/components/Container'
-import Typography from '@/components/Typography'
 import { algorithems } from '@/app/algorithms/data'
+import Algorithm from './Algorithm'
 
-export default function Algorithm({ params }) {
-    const [editorHeight, setEditorHeight] = useState('auto');
-
-    const handleEditorDidMount = (editor) => {
-        setTimeout(() => {
-            editor.getAction('editor.action.formatDocument').run();
-        }, 300);
-        editor.updateOptions({
-            scrollbar: {
-                vertical: 'hidden',
-                // horizontal: 'visible',
-                horizontal: 'hidden',
-                handleMouseWheel: false
-            },
-            wordWrap: 'off',
-            scrollBeyondLastLine: false,
-            mouseWheelScrollSensitivity: 0,
-            mouseWheelZoom: false
-        });
-
-        const updateHeight = () => {
-            const contentHeight = editor.getContentHeight();
-            setEditorHeight(`${contentHeight}px`);
-          };
-
-          updateHeight();
-    }
-      
-    return (
-        <AlgorithmContainer>
-            <SidePanel items={algorithems} />
-            <PageContainer column center>
-                <Typography variant="h2">{algorithems[params.title].title}</Typography>
-                {algorithems[params.title].content.map((item, index) => (
-                    <ContentContainer column key={index}>
-                        <Typography variant="body3" as='pre' >{item.description}</Typography>
-                        <Editor
-                            height={editorHeight}
-                            defaultLanguage="javascript"
-                            defaultValue={item.code}
-                            theme="vs-dark"
-                            onMount={handleEditorDidMount}
-                            options={{
-                                automaticLayout: true,
-                                formatOnPaste: true,
-                                scrollBeyondLastLine: false,
-                                fontSize: 15,
-                                minimap: { enabled: false },
-                                wordWrap: 'off',
-                                glyphMargin: false,
-                            }}
-                        />
-                    </ContentContainer>
-                ))}
-            </PageContainer>
-        </AlgorithmContainer>
-    )
+export let metadata = {
+    title: "Majid's Algorithms",
+    description: "Majid's Algorithms",
 }
 
-const AlgorithmContainer = Styled(Container)`
-`
-const PageContainer = Styled(Container)`
-    width: 80%;
-    margin: 0 auto;
-    h2 {
-        margin-bottom: 20px;
-        margin-top: 40px;
-    }
-    @media (max-width: 768px) {
-        width: 95%;
-    }
-`
-const ContentContainer = Styled(Container)`
-    width: 100%;
-    height: 100%;
-    > pre {
-        text-wrap: wrap;
-        margin: 20px 0;
-        white-space: pre-wrap; 
-    }
-    .monaco-editor .overlayWidgets .iPadShowKeyboard {
-        display: none;
-    }
-    .monaco-editor {
-        touch-action: none;
-    }
-`
+export default function AlgorithmPage({ params }) {
+    metadata.title = algorithems[params.title].title;
+    metadata.description = algorithems[params.title].title;
+
+    return (
+        <Algorithm params={params} />
+            
+    )
+}
